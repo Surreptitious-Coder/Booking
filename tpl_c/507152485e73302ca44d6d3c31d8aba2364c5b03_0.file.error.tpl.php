aa4a746974bd875f31de6a40457077d16519e641
Surreptitious-Coder <chrissyk99@hotmail.co.uk>
2021-08-06 09:38:58 +0100

    functionalised the addition

---
+++ tpl_c/507152485e73302ca44d6d3c31d8aba2364c5b03_0.file.error.tpl.php
@@ -0,0 +1,41 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2021-08-06 08:29:26
+  from '/var/www/html/tpl/error.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_610cf2e6343833_72593849',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '507152485e73302ca44d6d3c31d8aba2364c5b03' => 
+    array (
+      0 => '/var/www/html/tpl/error.tpl',
+      1 => 1628215355,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:globalheader.tpl' => 1,
+    'file:javascript-includes.tpl' => 1,
+    'file:globalfooter.tpl' => 1,
+  ),
+),false)) {
+function content_610cf2e6343833_72593849 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender('file:globalheader.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+<div class="error">
+    <h3><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0], array( array('key'=>$_smarty_tpl->tpl_vars['ErrorMessage']->value),$_smarty_tpl ) );?>
+</h3>
+    <h5><a href="//<?php echo $_SERVER['HTTP_HOST'];
+echo $_SERVER['REQUEST_URI'];?>
+"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['translate'][0], array( array('key'=>'ReturnToPreviousPage'),$_smarty_tpl ) );?>
+</a></h5>
+</div>
+
+<?php $_smarty_tpl->_subTemplateRender("file:javascript-includes.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+$_smarty_tpl->_subTemplateRender('file:globalfooter.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
